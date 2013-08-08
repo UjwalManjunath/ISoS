183c4b97f710a80316533909c68ecbff6923fba2
umanjunath <umanjunath@ubermind.com>
2013-08-08 13:06:56 -0600

    location updates added

--- ISoS/SoSEmergencyVC.h
+++ ISoS/SoSEmergencyVC.h
@@ -7,7 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface SoSEmergencyVC : UIViewController
+@interface SoSEmergencyVC : UIViewController <MFMessageComposeViewControllerDelegate,CLLocationManagerDelegate>
 
 @end
--- ISoS/SoSEmergencyVC.m
+++ ISoS/SoSEmergencyVC.m
@@ -9,7 +9,14 @@
 #import "SoSEmergencyVC.h"
 
 @interface SoSEmergencyVC ()
+
 @property (weak, nonatomic) IBOutlet UIImageView *crimeImage;  //using it to just transform the image
+
+@property(nonatomic,strong) CLLocationManager *locationManager;
+
+@property (nonatomic,strong) CLGeocoder *geocoder;
+
+@property(nonatomic,strong) CLPlacemark *placemark;
 
 @end
 
@@ -36,5 +43,72 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    switch (result) {
+        case MessageComposeResultCancelled:
+            NSLog(@"Cancelled");
+            break;
+        case MessageComposeResultFailed:{
+            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"emergency message" message:@"message sending failed" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert show];
+        }
+            break;
+            
+        case MessageComposeResultSent:
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations{
+    
+    CLLocation *currentLocation = [locations lastObject];
+    if (currentLocation != nil) {
+        NSLog(@" Longitude: %@",[NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude]);
+        NSLog(@"Latitude: %@",[NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude]);
+    }
+    
+ //   [self.locationManager stopUpdatingLocation];
+    
+    NSLog(@"Resolving the Address");
+    self.geocoder = [[CLGeocoder alloc]init];
+    [self.geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        if(error == nil && [placemarks count]>0){
+            self.placemark = [placemarks lastObject];
+            NSLog(@"Address : %@ %@ \n %@ %@ \n %@ \n %@",self.placemark.subThoroughfare,self.placemark.thoroughfare,self.placemark.postalCode,self.placemark.locality
+                  ,self.placemark.administrativeArea,self.placemark.country);
+        }
+        else {
+            NSLog(@"%@", error.debugDescription);
+        }
+    }];
+    
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.delegate =self;
+    self.locationManager.distanceFilter = kCLDistanceFilterNone ;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+
+    [self.locationManager startMonitoringSignificantLocationChanges];
+    NSLog(@"locationManager started updating location");
+}
+
+-(void) viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [self.locationManager stopMonitoringSignificantLocationChanges];
+}
+
+
+
 
 @end
